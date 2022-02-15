c811a31483f37efd89f5d9965016501777b04363
DavidMovsisyann <davitmovsisyan44@gmail.com>
2022-02-15 20:40:33 +0400

    sada

---
+++ Lesson1_Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson1_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your first name:");
+            var firstname = Console.ReadLine();
+            Console.WriteLine("Enter your last name:");
+            var lastname = Console.ReadLine();
+
+            Console.WriteLine($"User's first name is {firstname},user's last name is {lastname} ");
+        }
+    }
+}
